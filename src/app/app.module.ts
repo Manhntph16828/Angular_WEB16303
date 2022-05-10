9dbdfef8e172723597ee81e2cdc076c5a857659b
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2022-05-10 13:36:32 +0700

    component-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { AboutComponent } from './pages/about/about.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
